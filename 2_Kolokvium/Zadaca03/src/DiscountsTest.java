3d2d169716d0d9283e77747fa0908bcec7cda5d2
Borjan Najdoski <najdoskib@gmail.com>
2024-12-18 15:49:08 +0100

    Zadaca 3 delumno reshena

--- 2_Kolokvium/Zadaca03/src/DiscountsTest.java
+++ 2_Kolokvium/Zadaca03/src/DiscountsTest.java
@@ -1,10 +1,51 @@
+import java.io.InputStream;
 import java.util.*;
+import java.util.stream.Collectors;
 
 class Store {
 
     private String name;
-    private int average_disc;
+    private List<String> prices;
 
+    public Store() {
+        this.prices = new ArrayList<>();
+    }
+
+    public Store(String name, List<String> prices) {
+        this.name = name;
+        this.prices = prices;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<String> getPrices() {
+        return prices;
+    }
+
+    public void setPrices(List<String> prices) {
+        this.prices = prices;
+    }
+
+    public float calcualteAverageDiscount() {
+        float answer = 0;
+
+        for (String price : this.prices) {
+            String[] prices = price.split(":");
+            int discounted = Integer.parseInt(prices[0]);
+            int regular = Integer.parseInt(prices[1]);
+
+            answer += 100 - ((float) (discounted / regular) * 100);
+
+        }
+
+        return answer / this.prices.size();
+    }
 
     public String toString() {
         /*
@@ -18,12 +59,54 @@ class Store {
 }
 
 class Discounts {
-    public int readStores(InputStream inputStream) {
+    private List<Store> stores;
 
+    public Discounts() {
+        this.stores = new ArrayList<>();
+    }
+
+    public Discounts(List<Store> stores) {
+        this.stores = stores;
+    }
+
+    public List<Store> getStores() {
+        return stores;
+    }
+
+    public void setStores(List<Store> stores) {
+        this.stores = stores;
+    }
+
+    public int readStores(Scanner in) {
+
+        while (in.hasNext()) {
+            String line = in.nextLine().trim();
+
+            String[] storeInput = line.split(" ");
+
+            String store_name = storeInput[0];
+
+            ArrayList<String> store_prices = new ArrayList<>();
+            for (int i = 1; i < storeInput.length; i++) {
+                store_prices.add(storeInput[i]);
+            }
+
+            this.stores.add(new Store(store_name, store_prices));
+        }
+        return this.stores.size();
     }
 
     public List<Store> byAverageDiscount() {
-
+        List<Store> topDiscountStores = this.stores
+                .stream()
+                .sorted(
+                        Comparator
+                                .comparing(Store::calcualteAverageDiscount)
+                                .thenComparing(Store::getName)
+                )
+                .limit(3)
+                .toList();
+        return topDiscountStores;
     }
 
     public List<Store> byTotalDiscount() {
