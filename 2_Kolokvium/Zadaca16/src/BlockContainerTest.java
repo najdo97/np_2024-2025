c48c8f227e73554c3c91869ea2afa951f47a03da
Borjan Najdoski <najdoskib@gmail.com>
2025-08-05 17:46:05 +0200

    Pocetno resavanje zadaca 16

--- 2_Kolokvium/Zadaca16/src/BlockContainerTest.java
+++ 2_Kolokvium/Zadaca16/src/BlockContainerTest.java
@@ -1,6 +1,9 @@
 /*
-* Да се имплементира генеричка класа за блок контејнер BlockContainer. Контејнерот треба да има блоковска структура, со тоа што секој блок содржи N елементи. Контејнерот треба да ги задоволува следните услови:
+* Да се имплементира генеричка класа за блок контејнер BlockContainer. Контејнерот треба да има блоковска структура,
+*  со тоа што секој блок содржи N елементи.
+*
 
+Контејнерот треба да ги задоволува следните услови:
 константно време на пристап до секој блок $O(1)$
 логаритамско време на пристап до елементите во блокот $O(log N)$
 елементите во секој блок треба да бидат сортирани.
@@ -13,7 +16,60 @@ public void sort() - метод за сортирање на сите елеме
 public String toString() - препокривање на методот да враќа String во следниот формат: пример: [7, 8, 9],[1, 2, 3],[5, 6, 12],[4, 10, 8]
 * */
 
+
 import java.util.*;
+
+class Block<T> {
+
+    ArrayList<T> data;
+
+    public Block() {
+        this.data = new ArrayList<>();
+    }
+
+    int getDataSize() {
+        return data.size();
+    }
+
+    void addData(T value) {
+        this.data.add(value);
+    }
+
+}
+
+class BlockContainer<T> {
+    int max_size;
+    ArrayList<Block<T>> container;
+
+    public BlockContainer(int n) {
+        this.max_size = n;
+        this.container = new ArrayList<>();
+    } //- конструктор со еден аргумент, максималниот број на елементи во блокот
+
+    public void add(T a) {
+        if (container.get(container.size() - 1).getDataSize() < max_size) {
+            container.get(container.size() - 1).addData(a);
+        }else{
+            container.add(new Block<T>());
+            container.get(container.size() - 1).addData(a);
+        }
+
+    }//- метод за додавање елемент во последниот блок од контејнерот (ако блокот е полн, се додава нов блок)
+
+    public boolean remove(T a) {
+
+    }// - метод за бришње на елемент од последниот блок (ако се избришат сите елементи од еден блок, тогаш и блокот се брише)
+
+    public void sort() {
+
+    }// -    метод за сортирање на сите елементи во контејнерот
+
+    public String toString() {
+
+    } // -    препокривање на методот да враќа String во следниот формат:пример:[7,8,9],[1,2,3],[5,6,12],[4,10,8]
+
+
+}
 
 public class BlockContainerTest {
     public static void main(String[] args) {
@@ -23,7 +79,7 @@ public class BlockContainerTest {
         BlockContainer<Integer> integerBC = new BlockContainer<Integer>(size);
         scanner.nextLine();
         Integer lastInteger = null;
-        for(int i = 0; i < n; ++i) {
+        for (int i = 0; i < n; ++i) {
             int element = scanner.nextInt();
             lastInteger = element;
             integerBC.add(element);
@@ -37,7 +93,7 @@ public class BlockContainerTest {
         System.out.println(integerBC);
         BlockContainer<String> stringBC = new BlockContainer<String>(size);
         String lastString = null;
-        for(int i = 0; i < n; ++i) {
+        for (int i = 0; i < n; ++i) {
             String element = scanner.next();
             lastString = element;
             stringBC.add(element);
